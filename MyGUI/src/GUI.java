4c78cf36a90cb067278f5ea6df8ec1d771767361
astronautin <astronautin@users.noreply.github.com>
2020-10-18 13:19:55 +0200

    added gui

--- MyGUI/src/GUI.java
+++ MyGUI/src/GUI.java
@@ -1,4 +1,32 @@
+import java.awt.event.ActionListener;
 
-public class GUI {
+import javax.swing.*;
 
+public class GUI extends JFrame{
+	
+	private JFrame frame = new JFrame();
+	private JPanel panel = new JPanel();
+	private JLabel label = new JLabel();
+	private JButton button = new JButton("hit me");
+	
+	public GUI () {
+		init();
+	}
+	
+	private void init() {
+		panel.add(label);
+		panel.add(button);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(panel);
+		
+	}
+	
+	public void addButtonListener(ActionListener a) {
+		button.addActionListener(a);
+	}
+
+	public void setLabelText(String a) {
+		label.setText(a);
+	}
 }
