ac54bd23e6d37059a8cd0c5c73b0478506d2dc1c
astronautin <astronautin@users.noreply.github.com>
2020-10-18 13:18:24 +0200

    added GUI and GUI part of the controller

--- MyGUI/src/Controller.java
+++ MyGUI/src/Controller.java
@@ -1,4 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Controller {
 
+	private GUI gui = new GUI();
+	
+	public Controller(GUI gui) {
+		this.gui = gui;
+		
+		this.gui.addButtonListener(new ButtonListener());
+	}
+	
+	class ButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			gui.setLabelText("you hit me...");
+		}
+	}
 }
