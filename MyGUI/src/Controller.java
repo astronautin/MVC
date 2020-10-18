63e0d0ed15a74a9b9ac96c40a4f4164a8ff00022
astronautin <astronautin@users.noreply.github.com>
2020-10-18 13:26:59 +0200

    added the model

--- MyGUI/src/Controller.java
+++ MyGUI/src/Controller.java
@@ -4,16 +4,20 @@ import java.awt.event.ActionListener;
 public class Controller {
 
 	private GUI gui = new GUI();
+	private Model model = new Model();
 	
-	public Controller(GUI gui) {
+	public Controller(GUI gui, Model model) {
 		this.gui = gui;
+		this.model = model;
 		
 		this.gui.addButtonListener(new ButtonListener());
 	}
 	
+	
+	
 	class ButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			gui.setLabelText("you hit me...");
+			gui.setLabelText(Integer.toString(model.add2NUmbers()));
 		}
 	}
 }
--- MyGUI/src/Model.java
+++ MyGUI/src/Model.java
@@ -1,4 +1,13 @@
+import java.util.Random;
 
 public class Model {
+	
+	Random random = new Random();
+	
+	public int add2NUmbers() {
+		int a = random.nextInt(19);
+		int b = random.nextInt(19);
+		return a+b;
+	}
 
 }
